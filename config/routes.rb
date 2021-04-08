20abbffda971bf6525fd373bb726ee3fa42e8adc
Justin Hager <developerjustinhager@pm.me>
2021-04-07 17:38:00 -0700

    finish setting up pages route handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get requests for the pages path go to pages controller index method
+  get '/pages', to: 'pages#index'
 end
